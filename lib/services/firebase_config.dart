c82f17d0bdacfc56a8163c03e8cd9ceb0cc05262
Alan Luis Valenzuela Simpson <alan.v.simpson@hotmail.com>
2025-03-25 10:28:13 -0300

    google

---
+++ lib/services/firebase_config.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseConfig {
+  static Future<void> init() async {
+    try {
+      print('Iniciando inicialización de Firebase...');
+      
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyDO9f1l9fuv5kSzwmvfZkn2LJcfqRWD35U",
+          appId: "1:377740311156:ios:e2049f0d3e0389ee09af21",
+          messagingSenderId: "377740311156",
+          projectId: "paints-78769",
+          storageBucket: "paints-78769.firebasestorage.app",
+          iosClientId: "377740311156-n4o2p58h1plht8ajofq9p1b45epq1ndq.apps.googleusercontent.com",
+          iosBundleId: "com.example.miniaturePaintFinder"
+        ),
+      );
+      
+      print('Firebase inicializado exitosamente');
+    } catch (e, stackTrace) {
+      print('Error al inicializar Firebase: $e');
+      print('Stack trace: $stackTrace');
+      rethrow;
+    }
+  }
+} 
