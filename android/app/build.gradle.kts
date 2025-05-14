fffc674754515418e6a5290c961c944f11c5e4be
Angelo Sepulveda <angelo.sv@iqplus.no>
2025-05-14 11:15:37 +0200

    Corregir bundle ID a com.angelovs.miniaturePaintFinder

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.miniature_paint_finder"
+    namespace = "com.angelovs.miniaturePaintFinder"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.miniature_paint_finder"
+        applicationId = "com.angelovs.miniaturePaintFinder"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
